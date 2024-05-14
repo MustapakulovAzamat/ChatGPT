249b244213f91ffd8163b12dcc24cf3cc1ab732a
Mustapakulov Azamat <azamustapakylov6@gmail.com>
2024-05-14 23:59:49 +0600

    added modal

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import '../style/Chat.css'
 
 export const Chat = () => {
+  const [open, setOpen] = useState(false)
   return (
-    <div>Chat</div>
+    <>
+      <button onClick={() => {setOpen(!open)}}>Open Model</button>
+      <div className={`modal ${open ? 'open' : ''}`}>
+        <div className={`modal__content ${open ? 'open' : ''}`}>
+          <div>
+            <button onClick={() => {setOpen(!open)}}>X</button>
+          </div>
+          <ul>
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum neque praesentium atque, quibusdam officiis est reprehenderit officia assumenda. Reprehenderit et ipsa iure distinctio laudantium! Nemo cumque laborum impedit delectus consectetur.
+            </li>
+            
+            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum neque praesentium atque, quibusdam officiis est reprehenderit officia assumenda. Reprehenderit et ipsa iure distinctio laudantium! Nemo cumque laborum impedit delectus consectetur.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   )
 }
